d20626f74c9e2788d55a9f736b7b698eb0726895
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-10-03 09:15:29 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# This is the  program that print string in "UPPERCASE", followed by newline
+def uppercase(str):
+    for c in str:
+        if ord(c) in range(97, 123):
+            c = chr(ord(c) - 32)
+            print("{}".format(c), end="")
+            print("")
