bf602691bde81842dcfc974023cd49f04c1d3c81
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-10-05 13:33:19 -0700

    update

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-
-# a program that prints #pythoniscool, followed by a new line
-__import__("os").write(1,"#pythoniscool\n".encode("UTF-8"))
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
