de94eca4c721d818ebc4671976b3ac568c012532
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-10-05 13:41:33 -0700

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,8 +3,8 @@
 # This is a program that imports all functions from the file calculator_1.py and handles basic operations.
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-
     import sys
+    
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
