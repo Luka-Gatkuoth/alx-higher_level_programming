59d3d4dfbadf2e9cf76a496371c1c809c5cfad47
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-10-09 14:58:17 -0700

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""
-This is a  function that returns a 
-tuple with the length of a string and 
-its first character.
-"""
 def multiple_returns(sentence):
     if sentence == "":
         return (0, None)
