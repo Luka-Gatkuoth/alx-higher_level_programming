60f80a9c6d1bf9239dc2e50ec6064f8ca4eb0f29
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-10-12 04:01:33 -0700

    update

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# a function that returns the weighted average of all integers tuple 
+# a function that returns the weighted average of all integers tuple
 def weight_average(my_list=[]):
     if not my_list:
         return 0
