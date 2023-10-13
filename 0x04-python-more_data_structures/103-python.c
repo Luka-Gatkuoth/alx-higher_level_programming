de9d6c04fc01187e057218af58cb3045776609f0
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-10-13 05:13:14 -0700

    update

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -26,10 +26,12 @@ else
 limit = size + 1;
 printf(" first %ld bytes:", limit);
 for (i = 0; i < limit; i++)
+{
 if (string[i] >= 0)
 printf(" %02x", string[i]);
 else
 printf(" %02x", 256 + string[i]);
+}
 printf("\n");
 }
 /**
