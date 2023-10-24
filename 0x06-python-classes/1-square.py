b8f29849e397c30e625775969afcc81b375ccd6c
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-10-24 08:29:36 -0700

    task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,10 @@
 
 class Square:
     """Defines the square."""
-    def __init__(self, size):
+    pass
+
+
+def __init__(self, size):
     """Constructor.
     Args:
     size: length of side of square.
