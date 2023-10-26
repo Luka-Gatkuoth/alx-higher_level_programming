917ae2efa37aa473cdc8c77ecf65597a8e5dc73e
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-10-26 07:52:19 -0700

    update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Square module."""
+""" A module that represente the class Square """
 
 
 class Square:
-    """Defines the square."""
-    pass
-    
-    
-def __init__(self, size):
-    """Constructor.
-    Args:
-    size: length of side of square.
-    """
-    self.__size = size
+    """The class Square that defines square. """
+
+    def __init__(self, size) -> None:
+        """ Instantiation of the class Square
+        """
+        self.__size = size
