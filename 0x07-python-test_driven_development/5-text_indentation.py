e1add9577bafb2b8ed564fbd1c59ea15de294eb2
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2023-11-08 06:01:23 -0800

    Updates

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -15,9 +15,7 @@ def text_indentation(text):
     c = 0
     while c < len(text) and text[c] == ' ':
         c += 1
-
-
-    while c < len(text):
+        while c < len(text):
         print(text[c], end="")
         if text[c] == "\n" or text[c] in ".?:":
             if text[c] in ".?:":
