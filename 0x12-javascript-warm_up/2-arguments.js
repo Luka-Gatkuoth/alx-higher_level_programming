e9bcd3de6bc324beab608ddce1cc71bfda349a5c
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2024-06-18 16:06:37 -0700

    script that take argument from user

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 /* Script that take argument from user and print it */
-const args = process.argv.slice(2);
-if (args.length === 0) {
+const args = process.argv;
+if (args.length <= 2) {
   console.log('No argument');
-  process.exit(1);
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-const userInput = args[0];
-console.log(userInput);
