879384563c3b9241e353b5aec1ffa4b6abc667d3
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2024-06-19 08:20:42 -0700

    script that print multilines using loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/* script that print 3 lines using loops */
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i <= 2; i++) {
+  console.log(myVar[i]);
+}
