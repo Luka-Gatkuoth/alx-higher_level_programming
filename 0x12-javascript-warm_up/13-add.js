ed3109e45011eaa36c8099ed95ce356b8a6cfd77
Luka-Gatkuoth <lukagatkuoth4@gmail.com>
2024-06-20 16:51:55 -0700

    function add two number

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -2,6 +2,6 @@
 
 /* Script that print the addition of two number and function is vissible outside */
 
-exports.add = function add(a, b) {
+exports.add = function add (a, b) {
   return a + b;
-}
+};
